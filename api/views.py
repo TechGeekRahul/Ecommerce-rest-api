faaf0f449a55960858929a8fb9276090c403c41e
Rahul Kumar Singh <singhrahulkumar820@gmail.com>
2026-02-03 12:03:16 +0530

    created few tests and used a api view'

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from django.db.models import Max
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 class ProductListApiView(generics.ListCreateAPIView):
     queryset = Product.objects.exclude(stock__gt=0)
@@ -44,18 +46,30 @@ class OrderListApiView(generics.ListAPIView):
 class UserOrderListApiView(generics.ListAPIView):
         queryset = Order.objects.prefetch_related('items__product')
         serializer_class = OrderSerializer
+        permission_classes = [IsAuthenticated]
         
         def get_queryset(self):
             qs = super().get_queryset()
             return qs.filter(user=self.request.user)
-       
 
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products':products,
-        'count' : len(products),
-        'max_price':products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(serializer.data)
+class ProductInfoApiView(APIView):
+    def get(self,request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products':products,
+            'count' : len(products),
+            'max_price':products.aggregate(max_price=Max('price'))['max_price']
+        })
+        return Response(serializer.data)
+        
+           
+
+# @api_view(['GET'])
+# def product_info(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer({
+#         'products':products,
+#         'count' : len(products),
+#         'max_price':products.aggregate(max_price=Max('price'))['max_price']
+#     })
+#     return Response(serializer.data)
